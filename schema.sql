d187bdb7fcb73ce7f53cfb7bb7e4cb19b6c438a7
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-12-07 11:40:43 +0300

    feat: add the schema sql (question number 2)

---
+++ schema.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE Product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    creation_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Product_category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    product_id INT NOT NULL,
+    CONSTRAINT fk_product
+    FOREIGN KEY(product_id)
+    REFERENCES Product(id)
+    ON DELETE CASCADE
+);

24510be5cedbf27d5f93ed2c80076a88a8a791b1
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-12-07 10:41:48 +0300

    feat :  add crud permission to product_manager_user

--- init_db.sql
+++ init_db.sql
@@ -12,3 +12,7 @@ GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
 
 --ajout privilèges créations table
  GRANT CREATE ON DATABASE product_management_db TO product_manager_user;
+
+--ajout permissions CRUD pour product_manager_user
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO product_manager_user;

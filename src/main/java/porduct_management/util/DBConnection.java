0f7c8df88b01b6e3fd55d78a78e6abecdad39cf3
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-12-07 14:25:58 +0300

    feat: add DBconnection class (question nbr 5)

---
+++ src/main/java/porduct_management/util/DBConnection.java
@@ -0,0 +1,19 @@
+package porduct_management.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private static final String USER = "product_manager_user";
+    private static final String PASSWORD = "123456";
+    public static Connection getDBConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

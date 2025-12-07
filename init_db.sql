72552cede50c41258e3b4f01079a757d1214fdfe
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-12-07 13:16:56 +0300

    feat : add permission to sequence

--- init_db.sql
+++ init_db.sql
@@ -11,8 +11,14 @@ CREATE USER product_manager_user WITH PASSWORD '123456';
 GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
 
 --ajout privilèges créations table
- GRANT CREATE ON DATABASE product_management_db TO product_manager_user;
+GRANT CREATE ON DATABASE product_management_db TO product_manager_user;
+
+-- Droits sur le schéma public pour pouvoir créer des tables
+GRANT USAGE, CREATE ON SCHEMA public TO product_manager_user;
 
 --ajout permissions CRUD pour product_manager_user
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO product_manager_user;
+
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE product_id_seq TO product_manager_user;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE product_category_id_seq TO product_manager_user;
